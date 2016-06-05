e88b5c493cd5980b60ccc7965aa0b0486169435e
Deni <deni.munjas@gmail.com>
2016-06-05 03:15:07 +0200

    Began hardcore complement implementation. Recursion ends but there are some bugs in the implementation.

---
+++ src/espresso/Complement.java
@@ -0,0 +1,100 @@
+package espresso;
+
+import espresso.lockEnviroment.Cover;
+import espresso.lockEnviroment.Cube;
+
+import static espresso.InputState.ONE;
+import static espresso.InputState.ZERO;
+
+/**
+ * Complement
+ */
+public class Complement {
+  private static int fInputCount = 0;
+  private static int fOutputCount = 0;
+
+
+  public static Cover complement(Cover f, Cover d) {
+    if (f.size() == 0 || d.size() == 0)
+      throw new UnsupportedOperationException("One of the given covers are empty.");
+    if (f.inputCount() != d.outputCount() || f.outputCount() != d.outputCount())
+      throw new IllegalArgumentException("Given covers are not compatible. Input/output lengths are different");
+
+    Cover retValue = new Cover();
+
+    fInputCount = f.inputCount();
+    fOutputCount = f.outputCount();
+
+    for (int i = 0; i < fOutputCount; i++) {
+      Cover singleOutputF = extract(f, i);
+      Cover singleOutputD = extract(d, i);
+
+      retValue.addAll(singleOutputComplement(singleOutputF.union(singleOutputD)));
+    }
+
+    return retValue;
+  }
+
+  public static Cover singleOutputComplement(Cover f) {
+//    System.out.println(f);
+
+    //region Special cases
+    Cover retValue = new Cover();
+
+    if (f.hasDONTCARERow())
+      return retValue;
+
+    if (f.isUnate())
+      return UnateOperations.unateComplement(f);
+
+    Cube c = new Cube(f.inputCount(), f.outputCount());
+    boolean cubeChanged = false;
+    for (int i = 0; i < f.inputCount(); i++) {
+      if (f.getOneColumnCount(i) == f.size()) {
+        c.setInput(ONE, i);
+        cubeChanged = true;
+      }
+      else if (f.getZeroColumnCount(i) == f.size()) {
+        c.setInput(ZERO, i);
+        cubeChanged = true;
+      }
+    }
+
+    if (cubeChanged) {
+      retValue.add(c.copy().complement());
+      f = f.cofactor(c);
+    }
+    //endregion
+
+    int splitIndex = f.binateSelect();
+    Cube splitCube = new Cube(f.inputCount(), f.outputCount());
+    splitCube.setInput(ONE, splitIndex);
+
+    Cover[] cofactors = f.shannonCofactors(splitCube);
+
+    retValue.addAll(
+        f.mergeWithContainment(
+            singleOutputComplement(cofactors[1]),
+            singleOutputComplement(cofactors[0]),
+            splitCube,
+            false)
+    );
+
+    return retValue;
+  }
+
+  public static Cover extract(Cover cover, int outputIndex) {
+    Cover retValue = new Cover();
+
+    for (Cube cube : cover) {
+      Cube extraction = new Cube(fInputCount, 1); // Single output.
+
+      for (int i = 0; i < fInputCount; i++) extraction.setInput(cube.input(i), i);
+      extraction.setOutput(cube.output(outputIndex), 0);
+
+      retValue.add(extraction);
+    }
+
+    return retValue;
+  }
+}
