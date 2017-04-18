4879298f3cd8d81bf71d960521bb0a1937ab5d80
Deni <deni.munjas@gmail.com>
2017-04-18 03:16:20 +0200

    Add AuxiliaryMatrix calculation to Irredundant phase. Add minimal set calculation to Irredundant phase.

--- src/espresso/minimizers/espressoMinimizer/irredundant/Irredundant.java
+++ src/espresso/minimizers/espressoMinimizer/irredundant/Irredundant.java
@@ -3,11 +3,10 @@ package espresso.minimizers.espressoMinimizer.irredundant;
 
 import espresso.boolFunction.Cover;
 import espresso.boolFunction.cube.Cube;
-import espresso.urpAlgorithms.Tautology;
+import espresso.minimizers.espressoMinimizer.utils.BooleanMatrix;
 import espresso.utils.Pair;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static espresso.urpAlgorithms.Tautology.singleOutputTautologyCheck;
 
@@ -54,5 +53,62 @@ public class Irredundant {
     }
 
     return partiallyRedundant;
+  }
+
+  private static BooleanMatrix calculateAuxiliaryMatrix(Cover alpha, Cover beta) {
+    List<List<Integer>> minSets = calculateMinimalSets(alpha, beta, null);
+    return new AuxiliaryMatrix(minSets, alpha.size());
+  }
+
+  private static List<List<Integer>> calculateMinimalSets(
+      Cover alpha,
+      Cover beta,
+      List<Integer> alphaTrack
+  ) {
+    Cover alphaBetaUnion = alpha.union(beta);
+
+    if (alphaBetaUnion.isUnate()) {
+      List<Integer> minSet = new ArrayList<>();
+
+      if (beta.hasDONTCARERow()) {
+        return wrapInList(minSet);
+      }
+
+      for (int i = 0; i < alpha.size(); i++) {
+        if (alpha.get(i).isInputPartTautology()) {
+          int index = (alphaTrack != null ? alphaTrack.get(i) : i);
+          minSet.add(index);
+        }
+      }
+
+      return wrapInList(minSet);
+    } else {
+      int splitIndex = alphaBetaUnion.binateSelect();
+      Cube variable = alpha.generateVariableCube(splitIndex);
+      Cube complement = variable.copy().inputComplement();
+
+      Pair<Cover, List<Integer>> positiveAlphaPair = alpha.trackingCofactor(variable, alphaTrack);
+      Pair<Cover, List<Integer>> negativeAlphaPair = alpha.trackingCofactor(complement, alphaTrack);
+
+      Cover positiveBetaCofactor = beta.cofactor(variable);
+      Cover negativeBetaCofactor = beta.cofactor(complement);
+
+      List<List<Integer>> positiveSets =
+          calculateMinimalSets(positiveAlphaPair.first, positiveBetaCofactor, positiveAlphaPair.second);
+      List<List<Integer>> negativeSets =
+          calculateMinimalSets(negativeAlphaPair.first, negativeBetaCofactor, positiveAlphaPair.second);
+
+      List<List<Integer>> mergedSets = new ArrayList<>();
+      mergedSets.addAll(positiveSets);
+      mergedSets.addAll(negativeSets);
+
+      return mergedSets;
+    }
+  }
+
+  private static List<List<Integer>> wrapInList(List<Integer> minSet) {
+    List<List<Integer>> retValue = new ArrayList<>();
+    retValue.add(minSet);
+    return retValue;
   }
 }
